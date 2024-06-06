89d24d6958eb41f277e5e42bb5d91db83fca35d2
lukas-roth <82588709+lukas-roth@users.noreply.github.com>
2024-06-06 16:58:12 +0200

    Implement uid for server

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
 from collections import deque
+import datetime
 import json
 import socket
 import struct
@@ -8,11 +9,10 @@ import logging
 import logging.config
 import configparser
 import time
+import uuid
 from multicast_listener import MulticastListener
 
-# Load logging configuration
-logging.config.fileConfig('logging.conf', defaults={'logfilename': 'server.log'})
-logger = logging.getLogger()
+
 
 # Load configuration
 config = configparser.ConfigParser()
@@ -33,7 +33,13 @@ class ChatServer:
         self.last_100_messages = deque(maxlen=100)
         self.heartbeat_intervall = config.getint('SERVER','HeartbeatIntverval')
         self.timeout_multiplier = config.getint('SERVER','TimeoutMultiplier')
-        
+        self.uuid = uuid.uuid4()
+        self.uid = self.combine_timestamp_uuid('1970-01-01T00:00:00Z')
+
+        # Load logging configuration
+        logging.config.fileConfig('logging.conf', defaults={'logfilename': f'server-{self.uuid}.log'})
+        self.logger = logging.getLogger()
+        self.logger.info(f"Has uuid of {self.uuid}.")
 
     def announce(self):
         with socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP) as sock:
@@ -42,13 +48,13 @@ class ChatServer:
             while self.running:
                 message = f"{socket.gethostbyname(socket.gethostname())}:{self.server_port}"
                 sock.sendto(message.encode('utf-8'), (self.client_multicast_address, self.client_multicast_port))
-                logger.info(f"Announced server at {message}")
+                self.logger.info(f"Announced server at {message}")
                 time.sleep(5)  # Announce every 5 seconds
 
     def handle_client(self):
         with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as server_sock:
             server_sock.bind(('', self.server_port))
-            logger.info(f"Server listening on port {self.server_port}")
+            self.logger.info(f"Server listening on port {self.server_port}")
             while self.running:
                 try:
                     data, addr = server_sock.recvfrom(1024)
@@ -56,17 +62,17 @@ class ChatServer:
                         self.clients.append(addr)
                     if data:
                         message = data.decode('utf-8')
-                        logger.info(f"Received message from {addr}: {message}")
+                        self.logger.info(f"Received message from {addr}: {message}")
                         self.message_queue.put((message, addr))
                 except Exception as e:
-                    logger.error(f"Error handling client message: {e}")
+                    self.logger.error(f"Error handling client message: {e}")
 
 
     def consumer(self):
         while self.running:
             try:
                 message, addr = self.message_queue.get(timeout=1)
-                logger.info(f"Processing message: {message}")
+                self.logger.info(f"Processing message: {message}")
                 self.last_100_messages.append(message)
                 self.broadcast(self.last_100_messages, addr)
             except queue.Empty:
@@ -78,37 +84,66 @@ class ChatServer:
             for client in self.clients:
                 try:
                     sock.sendto(messages_str.encode('utf-8'), client)
-                    logger.info(f"Sent message to client {client}: {messages_str}")
+                    self.logger.info(f"Sent message to client {client}: {messages_str}")
                 except Exception as e:
-                    logger.error(f"Error sending message to client {client}: {e}")
+                    self.logger.error(f"Error sending message to client {client}: {e}")
 
     def send_heartbeat(self):
         with socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP) as sock:
             ttl = struct.pack('b', 1)
             sock.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, ttl)
             while self.running:
-                message = json.dumps(list(self.last_100_messages))
+                message = json.dumps({"data":list(self.last_100_messages), "time":datetime.datetime.now().isoformat()})
                 sock.sendto(message.encode('utf-8'), (self.server_multicast_address, self.server_multicast_port))
-                logger.info(f"Leader sent heartbeat: {message}")
+                self.logger.info(f"Leader sent heartbeat: {message}")
                 time.sleep(self.heartbeat_intervall)
 
     def listen_for_heartbeat(self, data):
         if isinstance(data, socket.timeout):
-            logger.warning("Heartbeat timeout! Initiating bully election.")
+            self.logger.warning("Heartbeat timeout! Initiating bully election.")
             # self.trigger_bully_election()
         else:
-            logger.info(f"Received heartbeat: {data}")
-            self.last_100_messages = deque(json.loads(data))
+            self.logger.info(f"Received heartbeat: {data}")
+            self.last_100_messages = deque(json.loads(data)["data"])
+            time = json.loads(data)["time"]
+            self.logger.debug(f"Time heartbeat: {time}")
+            self.uuid = self.combine_timestamp_uuid(time)
+    
+    def timestamp_to_int(self,timestamp):
+        """
+        Convert a timestamp to an integer representing milliseconds since the epoch.
+        """
+        dt = datetime.fromisoformat(timestamp)
+        epoch = datetime(1970, 1, 1)
+        return int((dt - epoch).total_seconds() * 1000)
+
+    def uuid_to_int(self,server_uuid):
+        """
+        Convert a UUID to an integer.
+        """
+        return uuid.UUID(server_uuid).int
+
+    def combine_timestamp_uuid(self,timestamp):
+        """
+        Combine the timestamp and UUID into a single integer.
+        """
+        timestamp_int = self.timestamp_to_int(timestamp)
+        uuid_int = self.uuid_to_int(self.uuid)
+        
+        # Shift timestamp_int to the left by 128 bits and add uuid_int
+        combined_int = (timestamp_int << 128) | uuid_int
+        return combined_int
 
     def run(self):
+        
         if self.is_replica: 
-            logger.debug('Am repilca, assuming duties.')
+            self.logger.debug('Am repilca, assuming duties.')
             # Listen for heatbeats and server data
             hearbeat_timeout = self.heartbeat_intervall * self.timeout_multiplier
-            heartbeat_listener = MulticastListener(self.server_multicast_address, self.server_multicast_port, self.listen_for_heartbeat, logger, True, hearbeat_timeout)
+            heartbeat_listener = MulticastListener(self.server_multicast_address, self.server_multicast_port, self.listen_for_heartbeat, self.logger, True, hearbeat_timeout)
             heartbeat_listener.start()
         else:
-            logger.debug('Am chat server, assuming duties.')
+            self.logger.debug('Am chat server, assuming duties.')
             # Start the hearbeat announcement thread
             heartbeat_thread = threading.Thread(target=self.send_heartbeat)
             heartbeat_thread.start()
@@ -128,10 +163,6 @@ class ChatServer:
             announce_thread.join()
             consumer_thread.join()
             heartbeat_thread.join()
-
-    
-
-            
 
 if __name__ == "__main__":
     import sys
